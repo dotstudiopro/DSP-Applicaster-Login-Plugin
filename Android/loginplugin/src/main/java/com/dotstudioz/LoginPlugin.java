6a7673bdb2d7c9cd97cae62fc902fb0261a10f91
Mohsin Shaikh <mohsin@dotstudioz.com>
2020-02-29 10:20:40 +0530

    Implemented HookScreenListener, HookScreen

--- Android/loginplugin/src/main/java/com/dotstudioz/LoginPlugin.java
+++ Android/loginplugin/src/main/java/com/dotstudioz/LoginPlugin.java
@@ -1,5 +1,6 @@
 package com.dotstudioz;
 
+import com.applicaster.hook_screen.HookScreen;
 import com.applicaster.hook_screen.HookScreenListener;
 import com.applicaster.plugin_manager.login.BaseLoginContract;
 import com.applicaster.plugin_manager.hook.HookListener;
@@ -11,19 +12,22 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginPlugin extends BaseLoginContract implements PluginScreen, HookScreenListener {
+public class LoginPlugin extends BaseLoginContract implements PluginScreen, HookScreen, HookScreenListener {
 
     private static String TAG = "LoginPlugin";
 
+    private HookListener hookListener;
     @Override
     public void executeOnApplicationReady(Context context, HookListener listener) {
         super.executeOnApplicationReady(context, listener);
+        this.hookListener = listener;
         Log.d(TAG, "executeOnApplicationReady: CALLED");
     }
 
@@ -66,5 +70,54 @@ public class LoginPlugin extends BaseLoginContract implements PluginScreen, Hook
     @Override
     public void hookFailed(@Nullable Map<String, Object> map) {
         Log.d(TAG, "hookFailed: CALLED");
+    }
+
+    
+
+    @NotNull
+    @Override
+    public HashMap<String, String> getHook() {
+        Log.d(TAG, "getHook: CALLED");
+        return null;
+    }
+
+    @Override
+    public void setHook(@NotNull HashMap<String, String> hashMap) {
+        Log.d(TAG, "setHook: CALLED");
+    }
+
+    @Override
+    public void executeHook(@NotNull Context context, @NotNull HookScreenListener hookScreenListener, @Nullable Map<String, ?> map) {
+        Log.d(TAG, "executeHook: CALLED");
+    }
+
+    @NotNull
+    @Override
+    public HookScreenListener getListener() {
+        Log.d(TAG, "getListener: CALLED");
+        return null;
+    }
+
+    @Override
+    public void hookDismissed() {
+        Log.d(TAG, "hookDismissed: CALLED");
+    }
+
+    @Override
+    public boolean isFlowBlocker() {
+        Log.d(TAG, "isFlowBlocker: CALLLED");
+        return false;
+    }
+
+    @Override
+    public boolean isRecurringHook() {
+        Log.d(TAG, "isRecurringHook: CALLED");
+        return false;
+    }
+
+    @Override
+    public boolean shouldPresent() {
+        Log.d(TAG, "shouldPresent: CALLED");
+        return false;
     }
 }
